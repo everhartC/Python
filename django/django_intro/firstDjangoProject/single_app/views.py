512c275b3a9f020a91714ecdf093c82a9dc67922
Cameron Everhart <cameron.r.everhart@gmail.com>
2021-02-09 22:57:37 -0800

    First Django project added

---
+++ django/django_intro/firstDjangoProject/single_app/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, HttpResponse, redirect
+
+
+def index(request):
+    return HttpResponse("placeholder to later display a list of all blogs")
+
+def new(request):
+    return HttpResponse("placeholder to display a new form to create a new blog")
+
+def create(request):
+    response = redirect('/')
+    return response
+
+def show(request, number):
+    return HttpResponse(f"placeholder to display blog number: {number}")
+
+def edit(request, number):
+    return HttpResponse(f"placeholder to edit blog {number}")
+
+def destroy(request, number):
+    response = redirect('/')
+    return response
+# Create your views here.
