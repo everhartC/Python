fd6d290c976eaadeec9d4544019e1f43fec441cc
Cambo <cameron.r.everhart@gmail.com>
2021-02-24 09:29:14 -0800

    Delete django/django_orm/week7LectureDemo directory

--- django/django_orm/week7LectureDemo/demoApp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, HttpResponse
-from .models import *
-
-# Create your views here.
-def index(request):
-    context = {
-        'Users': User.objects.all(),
-    }
-    return render(request, 'home.html', context)
