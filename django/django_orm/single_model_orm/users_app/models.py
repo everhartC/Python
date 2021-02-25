df825d650dc5f5f9113879afe8ad6903c4f50196
Cameron Everhart <cameron.r.everhart@gmail.com>
2021-02-24 16:32:31 -0800

    Adding the Users (Shell) django assignment

---
+++ django/django_orm/single_model_orm/users_app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Users(models.Model):
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    age = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __repr__(self):
+        return f"User Name: {self.fname} {self.lname}"
